14ef70b9f14215ef877545e2c97aba010da11fe1
sanwarhossainkhan <sanowr.khan@gmail.com>
2023-08-26 17:07:09 +0600

    api route delete

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::post('/call-python-script', [PythonController::class, 'callPythonScript']);
+//
+//Route::post('/call-python-script', [PythonController::class, 'callPythonScript']);
