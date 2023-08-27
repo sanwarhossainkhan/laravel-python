fa015fef6248352aa0f760fe4482815c7dd755eb
sanwarhossainkhan <sanowar.khan@gmail.com>
2023-08-27 11:33:25 +0600

    fix problem

--- app/Http/Controllers/PythonController.php
+++ app/Http/Controllers/PythonController.php
@@ -15,7 +15,7 @@ class PythonController extends Controller
         $age = $request->age;
         $output = shell_exec("python C:\laragon\www\laravel-python\python-script\hello.py \"$name\" \"$age\""); // change this path
         return response()->json(trim($output));
-//        var_dump($output);
+//        var_dump($output);  // error checking
   }
 
 }
